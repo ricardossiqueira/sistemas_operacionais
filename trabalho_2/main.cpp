b6a471a6a9cb52ce8d4b8fcade498f8097ac3759
ricardossiqueira <ricardosantossiqueira@poli.ufrj.br>
2021-09-08 20:09:01 -0300

    🔧 V0.1 new class error

--- trabalho_2/main.cpp
+++ trabalho_2/main.cpp
@@ -1,50 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <unistd.h>
 #include <mutex>
 #include <thread>
 
 using namespace std;
 
-class BarberShop
-{
-private:
-  const unsigned _totalChairs = 4;
-  unsigned _usingChairs = 0;
-  mutex mtx;
+const unsigned totalChairs = 4;
+unsigned usingChairs = 0;
+mutex mtx;
 
-public:
-  void haircut();
-};
-
-void BarberShop::haircut()
+auto haircut = []()
 {
-  lock_guard<mutex> lockGuard(mtx);
-  // mtx.lock();
-  if (_totalChairs - _usingChairs > 0)
+  if (totalChairs - usingChairs > 0)
   {
-    _usingChairs++;
-    cout
-        << "Cortando cabelo... <thread_id: "
-        << this_thread::get_id()
-        << ">"
-        << endl;
-    _usingChairs--;
+    mtx.lock();
+    usingChairs++;
+    mtx.unlock();
+
+    usleep(1);
+    cout << "Cortando cabelo...\n";
+
+    mtx.lock();
+    usingChairs--;
+    mtx.unlock();
   }
-  // mtx.unlock();
-}
+  else
+    cout << "Salao cheio, saindo...\n";
+};
 
 int main(int argc, char *argv[])
 {
   vector<thread> queue;
-  BarberShop barberShop;
 
   for (int i = 0; i < 10; i++)
-  {
-    queue.push_back(thread(&BarberShop::haircut, &barberShop));
-  }
-
-  for (auto &t : queue)
-    t.join();
+    new thread(haircut);
 
   return 0;
 }
